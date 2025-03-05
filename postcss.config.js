9acbce9f53054ea00c44ed5ce171ff72e1b988c6
2NG CODER <nahomnigussie7@gmail.com>
2025-03-05 17:01:41 +0300

    install tailwindcss

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+// filepath: /c:/Users/NAHOM/OneDrive/Desktop/Personal website/et-marketplace/postcss.config.js
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
